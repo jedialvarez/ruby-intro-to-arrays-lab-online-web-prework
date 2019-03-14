2b8a12bdcc8ce12064415ec3e69caa4f300395f7
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-14 17:17:44 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,26 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  elements = ["water", "fire"]
+end
+
+def first_element
+end
+
+def third_element
+  elements[3]
+end
+
+def last_element()
+end
+
+def first_element_with_array_methods()
+end
+
+def last_element_with_array_methods()
+end
+
+def length_of_array()
+end
